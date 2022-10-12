1705ff4bd77c25179eaf30887a4bd5626da32806
yhimanka <n1ylto00@students.oamk.fi>
2022-10-12 19:03:48 +0300

    edits

--- src/App.js
+++ src/App.js
@@ -1,91 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useState} from 'react';
+
+
+const URL = 'https://api.exchangerate.host/latest'
 
 function App() {
-  const [weight, setWeight] = useState(90)
-  const [bottles, setBottles] = useState(0)
-  const [time, setTime] = useState(0)
-  const [gender, setGender] = useState('male')
-  const [result, setResult] = useState(0)
+  const [eur, setEur] = useState(0);
+  const [gbp, setGbp] = useState(0);
+  const [rate, setRate] = useState (0);
 
-  function handleSubmit(e) {
-    e.preventDefault()
 
-    let grams = (bottles * 0.33) * 8 * 4.5
-    let burning = weight / 10
-    let gramsLeft = grams - (burning * time)
-    let alcoLevel = 0
-    
-    if (gender === 'male') {
-      alcoLevel = gramsLeft / (weight * 0.7)
+  async function convert(e) {
+    e.preventDefault();
+    try {
+      const address = URL;
+      const response = await fetch(address);
+
+    if (response.ok) {
+      const json = await response.json();
+      setRate(json.rates.GBP);
+      setGbp(eur * json.rates.GBP);
     } else {
-      alcoLevel = gramsLeft / (weight * 0.6)
-    } if (alcoLevel < 0) {
-      setResult(0)
-    } else {
-      setResult(alcoLevel);
+      alert('Error retrieving exchange rate.');
+    }
+
+  } catch (err) {
+    alert(err);
     }
   }
+
+
   return (
-    <>
-      <h3>Calculating alcohol blood level</h3>
-      <form onSubmit={handleSubmit}>
+    <div id='container'>
+      <form onSubmit={convert}>
         <div>
-          <label>Weight</label>
-          <input name="weight" type="number" value={weight} onChange={e => setWeight(e.target.value)}></input>
+          <label>Eur</label>&nbsp;
+          <input type= "number" step="0.01" 
+          value={eur} onChange={e => setEur(e.target.value)}/>
+          <output>{rate}</output>
         </div>
         <div>
-          <label>Bottles</label>
-          <select name="bottles" value={bottles} onChange={e => setBottles(e.target.value)} >
-            <option value="0">0</option>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-            <option value="7">7</option>
-            <option value="8">8</option>
-            <option value="9">9</option>
-            <option value="10">10</option>
-            <option value="11">11</option>
-            <option value="12">12</option>
-            <option value="13">13</option>
-            <option value="14">14</option>
-          </select>
+          <label>Gbp </label>
+          <output>{gbp.toFixed(2)} €</output>
         </div>
         <div>
-          <label>Time</label>
-          <select name="time" value={time} onChange={e => setTime(e.target.value)} >
-            <option value="0">0</option>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-            <option value="7">7</option>
-            <option value="8">8</option>
-            <option value="9">9</option>
-            <option value="10">10</option>
-            <option value="11">11</option>
-            <option value="12">12</option>
-            <option value="13">13</option>
-            <option value="14">14</option>
-          </select>
+          <button>Calculate</button>
         </div>
-        <div>
-          <label>Gender</label>
-          <input type="radio" name="gender" value="male" defaultChecked onChange={e => setGender(e.target.value)}></input><label>Male</label>
-          <input type="radio" name="gender" value="female" onChange={e => setGender(e.target.value)}></input><label>Female</label>
-        </div>
-        <div>
-          <output>Your alcohol blood level is (BAC) {result.toFixed(2)}</output>
-        </div>
-        <button>Calculate</button>
+
       </form>
-    </>
+
+    </div>
   );
 }
 
